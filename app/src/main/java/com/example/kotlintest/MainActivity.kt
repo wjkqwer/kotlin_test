3c5d7a0de5ffd2fd29057a7da899f586c80005a0
WJK <k8895@163.com>
2020-05-25 10:59:44 +0800

    commit

--- app/src/main/java/com/example/kotlintest/MainActivity.kt
+++ app/src/main/java/com/example/kotlintest/MainActivity.kt
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.*
+import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -83,10 +87,37 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    /**
-     * SQLite
-     */
+    private fun sendRequestWithHttpURLConnection() {
+        thread {
+            var connection: HttpURLConnection? = null
+            try {
+                val response=StringBuilder()
+                val url= URL("https://www.baidu.com")
+                connection=url.openConnection() as HttpURLConnection
+                connection.connectTimeout=8000
+                connection.readTimeout=8000
+                val input=connection.inputStream
+                //对获取到的输入流进行读取
+                val reader=BufferedReader(InputStreamReader(input))
+                reader.use {
+                    reader.forEachLine {
+                        response.append(it)
+                    }
+                }
+                showResponse(response.toString())
+            }catch (e:Exception){
+                e.printStackTrace()
+            }finally {
+                connection?.disconnect()
+            }
+        }
+    }
 
+    private fun showResponse(response:String){
+        runOnUiThread {
+            text_hello.text = response
+        }
+    }
 }
 
 
