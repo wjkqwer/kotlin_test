fcf346638c5cf374e0839608e877826fe751f76d
WJK <k8895@163.com>
2020-06-02 21:35:10 +0800

    add jetpack test

--- app/src/main/java/com/example/kotlintest/jetpack/MyObserver.kt
+++ app/src/main/java/com/example/kotlintest/jetpack/MyObserver.kt
@@ -8,7 +8,8 @@ import androidx.lifecycle.OnLifecycleEvent
 /**
  * Created by WJK On 2020/6/2
  */
-class MyObserver(val lifecycle:Lifecycle) : LifecycleObserver {
+//class MyObserver(val lifecycle:Lifecycle) : LifecycleObserver {
+class MyObserver : LifecycleObserver {
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun activityStart() {
